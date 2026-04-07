68ab60e43f3201db96471da11dd7dbf9650ce366
rafifpcr <m.rafif24si@mahasiswa.pcr.ac.id>
2026-04-07 16:26:47 +0700

    Pertemuan 4  Responsive & Grid Design

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -2,11 +2,12 @@ import { createRoot } from "react-dom/client";
 import FrameworkList from "./frameworkList";
 import "./tailwind.css";
 import FrameworkListSearchFilter from "./frameworkListSearchFilter";
-
+import ResponsiveDesign from "./ResponsiveDesign";
 
 createRoot(document.getElementById("root"))
     .render(
         <div>
-            <FrameworkListSearchFilter />
+            <ResponsiveDesign />
+            {/* <FrameworkListSearchFilter /> */}
         </div>
     )
